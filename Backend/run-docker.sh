1c9b9f0708c399138e12d3aac922ce1f951a0bf0
Michel, Florian <florian.michel@axelspringer.de>
2017-04-04 14:10:20 +0200

    edit docker run file

--- Backend/run-docker.sh
+++ Backend/run-docker.sh
@@ -1,5 +1,4 @@
 #/bin/bash
 
-
-docker run --rm -it -v /Users/fmichel/Desktop/test/GeburtstagsWebApp/Backend/GeburtztagsKalender:/GebKalenderBackend/ -p 8080:8080 --name geburtstagskalender --link geburtstagskalenderdb geburtstagskalenderbackend:2.0
+#docker run --rm -it -v $(pwd)/GeburtztagsKalender:/GebKalenderBackend/ -p 8080:8080 --name geburtstagskalender --link geburtstagskalenderdb geburtstagskalenderbackend:2.0
 
--- DB_Container/run-docker.sh
+++ DB_Container/run-docker.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 
-docker run --rm -it -v /Users/fmichel/Desktop/test/GeburtstagsWebApp_Kopie/DB_Container/Database:/var/lib/mysql/ -v /Users/fmichel/Desktop/test/GeburtstagsWebApp_Kopie/DB_Container//log:/var/log/mysql -p 3306:3306 --name  geburtstagskalenderdb geburtstagskalenderdb:2.0
+docker run --rm -it -v $(pwd)/Database:/var/lib/mysql/ -v $(pwd)/log:/var/log/mysql -p 3306:3306 --name  geburtstagskalenderdb geburtstagskalenderdb:2.0
 
